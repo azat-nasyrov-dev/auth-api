6454657cd3ab77ec2e693d9eb078ccacd0e66e8a
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-04-15 17:30:15 +0600

    added prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  public async onModuleInit() {
+    await this.$connect();
+  }
+
+  public async enableShutdownHooks(app: INestApplication) {
+    // @ts-ignore
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
